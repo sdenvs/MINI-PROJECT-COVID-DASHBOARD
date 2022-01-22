b07cbcb85f69efdc815e402bf967e04194cbeae2
sdenvs <sdenvs@gmail.com>
2022-01-22 13:46:17 +0530

    testCases-3

--- src/components/TablesList/index.js
+++ src/components/TablesList/index.js
@@ -1,6 +1,6 @@
 import {Component} from 'react'
 import {FcGenericSortingAsc, FcGenericSortingDesc} from 'react-icons/fc'
-import {Link} from 'react-router-dom'
+import TotalStats from '../TotalStats'
 import './index.css'
 
 class RenderTable extends Component {
@@ -22,59 +22,51 @@ class RenderTable extends Component {
       details.reverse()
     }
     return (
-      <div className="alignTableCenter">
-        <div
-          testid="stateWiseCovidDataTable"
-          className="tableBorder text-center"
-        >
-          <table>
-            <tr className="bottomBorder">
-              <th className="stateName">
-                <div className="d-flex align-items-center">
-                  <p>States/UT</p>
-                  <div testid="stateWiseCovidDataTable">
-                    <button
-                      onClick={this.changeToAce}
-                      type="button"
-                      testid="ascendingSort"
-                    >
-                      <FcGenericSortingAsc className="tableIcon" />
-                    </button>
-                    <button
-                      onClick={this.changeToDec}
-                      type="button"
-                      testid="descendingSort"
-                    >
-                      <FcGenericSortingDesc className="tableIcon" />
-                    </button>
-                  </div>
-                </div>
-              </th>
-              <th className="count">Confirmed</th>
-              <th className="count">Active</th>
-              <th className="count">Recovered</th>
-              <th className="count">Deceased</th>
-              <th className="count">Population</th>
-            </tr>
-
-            {details.map(eachItem => (
-              <tr className="highLIght">
-                <td className="stateNameColumn">
-                  <Link
-                    to={`/state/${eachItem.stateCode}`}
-                    className="colorName"
-                  >
-                    {eachItem.name}
-                  </Link>
-                </td>
-                <td className="Confirmed">{eachItem.confirmed}</td>
-                <td className="Active">{eachItem.active}</td>
-                <td className="Recovered">{eachItem.recovered}</td>
-                <td className="Deceased">{eachItem.deceased}</td>
-                <td className="population">{eachItem.population}</td>
-              </tr>
-            ))}
-          </table>
+      <div className="all-states-table" testid="stateWiseCovidDataTable">
+        <div className="overflow-container">
+          <div className="table-header">
+            <div className="state-name-heading">
+              <p className="table-header-title ">States/UT</p>
+              <button
+                className="order"
+                type="button"
+                testid="ascendingSort"
+                onClick={this.changeToAce}
+              >
+                <FcGenericSortingAsc className="order-icon" />
+              </button>
+              <button
+                className="order"
+                type="button"
+                testid="descendingSort"
+                onClick={this.changeToDec}
+              >
+                <FcGenericSortingDesc className="order-icon" />
+              </button>
+            </div>
+            <div className="other-tables-bar">
+              <p className="table-header-title">Confirmed</p>
+            </div>
+            <div className="other-tables-bar">
+              <p className="table-header-title">Active</p>
+            </div>
+            <div className="other-tables-bar">
+              <p className="table-header-title">Recovered</p>
+            </div>
+            <div className="other-tables-bar">
+              <p className="table-header-title">Deceased</p>
+            </div>
+            <div className="other-tables-bar">
+              <p className="table-header-title">Population</p>
+            </div>
+          </div>
+          <div className="state-wise-data-container">
+            <ul className="other-tables">
+              {details.map(each => (
+                <TotalStats key={each.stateCode} data={each} />
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     )
---
+++ src/components/TotalStats/index.js
@@ -0,0 +1,37 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const TotalStats = props => {
+  const {data} = props
+  const {name, confirmed, recovered, deceased, population, stateCode} = data
+  const active = confirmed - recovered - deceased
+
+  return (
+    <>
+      <li className="list-all-cases ">
+        <div className="states-container-home">
+          <Link to={`/state/${stateCode}`} className="link-home">
+            <p className="states-names-home">{name}</p>
+          </Link>
+        </div>
+        <div className="home-columns">
+          <p className="confirmed-home">{confirmed}</p>
+        </div>
+        <div className="home-columns">
+          <p className="active-home">{active}</p>
+        </div>
+        <div className="home-columns">
+          <p className="recovered-home">{recovered}</p>
+        </div>
+        <div className="home-columns">
+          <p className="deceased-home">{deceased}</p>
+        </div>
+        <div className="home-columns">
+          <p className="population-home">{population}</p>
+        </div>
+      </li>
+    </>
+  )
+}
+
+export default TotalStats
